087985c2e3c9c5df47a9f6a2f6073139c0b69170
Antonina Sych <antoninasych@gmail.com>
2024-11-17 16:25:34 +0000

    tasks  1-5

---
+++ task1/query.sql
@@ -0,0 +1,13 @@
+-- 1. Напишіть SQL команду, за допомогою якої можна:
+--
+-- вибрати всі стовпчики (За допомогою wildcard “*”) з таблиці products;
+-- вибрати тільки стовпчики name, phone з таблиці shippers,
+-- та перевірте правильність її виконання в MySQL Workbench.
+
+-- Вибір всіх стовпчиків з таблиці products
+SELECT *
+FROM products;
+
+-- Вибір стовпчиків name, phone з таблиці shippers
+SELECT name, phone
+FROM shippers;
